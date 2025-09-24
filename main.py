bac1bb50c1f0bfae4b89ac2c4b632034aa190959
Jeremiah Nnamchi (shxtic) <shxtic@gmail.com>
2025-09-24 22:44:19 +0200

    Clean up code formatting and style

--- main.py
+++ main.py
@@ -3,38 +3,37 @@ from stats import count_characters
 from stats import sort_num_characters
 import sys
 
+
 def get_book_text(file):
     with open(file) as f:
         book_str = f.read()
     return book_str
 
 
-
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
-    else: pass
-# Path to book
+    else:
+        pass
+        # Path to book
     book_path = sys.argv[1]
-# getting books entire contents
+    # getting books entire contents
     book_contents = get_book_text(book_path)
 
     num_of_words = count_words(book_contents)
 
-    print(f"Found {num_of_words} total words")
-
-    num_of_characters =count_characters(book_contents)
+    num_of_characters = count_characters(book_contents)
     list_of_sorted_dict = sort_num_characters(num_of_characters)
 
-#Print Output
+    # Print Output
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_of_words} total words")
     print("--------- Character Count -------")
 
-    for i in range(0,len(list_of_sorted_dict)):
+    for i in range(0, len(list_of_sorted_dict)):
         char = ""
         numb = int
 
@@ -43,10 +42,9 @@ def main():
         char = dict_cache["char"]
         if char.isalpha() == True:
             print(f"{char}: {numb}")
-        else: pass
+        else:
+            pass
     print("============= END ===============")
-
-
 
 
 main()
